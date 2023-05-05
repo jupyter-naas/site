1aac0f32ff2060ea398826819ca55eb4e0a9bffa
bijo-babu <bijo.babu@bridge-global.com>
2023-05-05 17:27:59 +0530

    feat: 5-reproduce-the-current-pricing-page

--- site/src/pages/pricing.js
+++ site/src/pages/pricing.js
@@ -288,7 +288,9 @@ export default function Pricing() {
                                     Shared resources
                                 </li>
                             </ul>
-                            <Link className={clsx(styles.btnGreen, styles.btn)} to="https://www.naas.ai/free-forever">CREATE ACCOUNT</Link>
+                            <Link className={clsx(styles.btnGreen, styles.btn)} to="https://www.naas.ai/free-forever">CREATE ACCOUNT
+                            <svg width="1em" viewBox="0 0 15 12" xmlns="http://www.w3.org/2000/svg"><path d="M9.6 7H1a1 1 0 1 1 0-2h8.6L7 2.4A1 1 0 0 1 8.4 1l4.3 4.2c.2.3.3.5.3.8 0 .3-.1.5-.3.7L8.4 11A1 1 0 1 1 7 9.5L9.6 7z" fill="currentColor"></path></svg>
+                            </Link>
                         </div>
                         <div className={clsx(styles.gridItem, styles.pricingTier)}>
                             <h3>Professional</h3>
