b6e9a31bf6a1d59821b2520e0f40d6a263f0ca10
srini047 <srinikethcr7@gmail.com>
2023-05-31 19:41:29 +0530

    remove: commented code

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -67,202 +67,183 @@ const config = {
       colorMode: {
         disableSwitch: true,
       },
-      // navbar: {
-      //   style: "dark",
-      //   title: "",
-      //   // For the scrollbar to be sticky or hide while scrolled down
-      //   // hideOnScroll: true,
-      //   logo: {
-      //     alt: "Naas v2 Documentation",
-      //     src: "https://landen.imgix.net/jtci2pxwjczr/assets/5ice39g4.png?w=186",
-      //   },
+      navbar: {
+        style: "dark",
+        title: "",
+        // For the scrollbar to be sticky or hide while scrolled down
+        // hideOnScroll: true,
+        logo: {
+          alt: "Naas v2 Documentation",
+          src: "https://landen.imgix.net/jtci2pxwjczr/assets/5ice39g4.png?w=186",
+        },
+        items: [
+          {
+            type: "dropdown",
+            label: "How it works",
+            position: "left",
+            items: [
+              {
+                type: "doc",
+                label: "Getting started",
+                docId: "getting-started/installation",
+              },
+              {
+                type: "doc",
+                label: "Core Components",
+                docId: "core-components/jobs/scheduler",
+              },
+              {
+                type: "doc",
+                label: "Platform",
+                docId: "platform/analytics-dashboard-deployment",
+              },
+            ],
+          },
+          {
+            type: "dropdown",
+            label: "Solutions",
+            position: "left",
+            items: [
+              {
+                type: "doc",
+                label: "Foundations Engines & Plugins",
+                docId: "solutions/sales-engine",
+              },
+              {
+                type: "doc",
+                label: "Content Creation",
+                docId: "solutions/content-engine",
+              },
+              {
+                type: "doc",
+                label: "Growth Marketing",
+                docId: "solutions/growth-engine",
+              },
+              {
+                type: "doc",
+                label: "Sales Conversion",
+                docId: "solutions/sales-engine",
+              },
+              {
+                type: "doc",
+                label: "Operations Efficiency",
+                docId: "solutions/operations-engine",
+              },
+              {
+                type: "doc",
+                label: "Finance Management",
+                docId: "solutions/finance-engine",
+              },
+              {
+                type: "doc",
+                label: "Open Data Intelligence",
+                docId: "solutions/opendata-engine",
+              },
+              {
+                type: "doc",
+                label: "Expert Acceleration Program",
+                docId: "solutions/operations-engine",
+              },
+            ],
+          },
+          {
+            type: "dropdown",
+            label: "Resources & Support",
+            position: "left",
+            items: [
+              {
+                to: "/blog",
+                label: "Blog",
+              },
+              {
+                type: "doc",
+                label: "Naas University",
+                docId: "solutions/operations-engine",
+              },
+              {
+                type: "doc",
+                label: "Open Source Community",
+                docId: "solutions/operations-engine",
+              },
+              {
+                type: "doc",
+                label: "Customer Stories",
+                docId: "solutions/operations-engine",
+              },
+              {
+                type: "doc",
+                label: "Join our Slack",
+                docId: "solutions/operations-engine",
+              },
+              {
+                type: "doc",
+                label: "Contact Support",
+                docId: "solutions/operations-engine",
+              },
+            ],
+          },
+          {
+            href: "/pricing",
+            label: "Pricing",
+            position: "left",
+          },
+          {
+            type: "html",
+            position: "right",
+            value: `<a href="https://workspace.naas.ai/login" target="_blank"><button>Sign in</button><a>`,
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Docs",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/jupyter-naas/naas",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} NaasAI, Inc.`,
+      },
+      //   navbar: {
       //   items: [
       //     {
-      //       type: "dropdown",
-      //       label: "How it works",
-      //       position: "left",
-      //       items: [
-      //         {
-      //           type: "doc",
-      //           label: "Getting started",
-      //           docId: "getting-started/installation",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Core Components",
-      //           docId: "core-components/jobs/scheduler",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Platform",
-      //           docId: "platform/analytics-dashboard-deployment",
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       type: "dropdown",
-      //       label: "Solutions",
-      //       position: "left",
-      //       items: [
-      //         {
-      //           type: "doc",
-      //           label: "Foundations Engines & Plugins",
-      //           docId: "solutions/sales-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Content Creation",
-      //           docId: "solutions/content-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Growth Marketing",
-      //           docId: "solutions/growth-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Sales Conversion",
-      //           docId: "solutions/sales-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Operations Efficiency",
-      //           docId: "solutions/operations-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Finance Management",
-      //           docId: "solutions/finance-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Open Data Intelligence",
-      //           docId: "solutions/opendata-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Expert Acceleration Program",
-      //           docId: "solutions/operations-engine",
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       type: "dropdown",
-      //       label: "Resources & Support",
-      //       position: "left",
-      //       items: [
-      //         {
-      //           to: "/blog",
-      //           label: "Blog",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Naas University",
-      //           docId: "solutions/operations-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Open Source Community",
-      //           docId: "solutions/operations-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Customer Stories",
-      //           docId: "solutions/operations-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Join our Slack",
-      //           docId: "solutions/operations-engine",
-      //         },
-      //         {
-      //           type: "doc",
-      //           label: "Contact Support",
-      //           docId: "solutions/operations-engine",
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       href: "/pricing",
-      //       label: "Pricing",
-      //       position: "left",
-      //     },
-      //     {
-      //       href: "https://workspace.naas.ai/search",
-      //       label: "Search",
-      //       position: "right",
-      //       color: "dark",
-      //     },
-      //     {
-      //       to: "https://workspace.naas.ai/login/",
-      //       label: "Login",
-      //       position: "right",
-      //       color: "dark",
-      //       target: "_blank",
-      //     },
-      //     {
-      //       to: "https://workspace.naas.ai/signup/",
-      //       label: "Signup",
-      //       position: "right",
-      //       color: "primary",
-      //       target: "_blank",
-      //     },
-      //     {
-      //       type: "search",
-      //       position: "right",
+      //       type: 'html',
+      //       position: 'right',
+      //       value: `<a href="https://workspace.naas.ai/login" target="_blank"><button>Sign in</button><a>`,
       //     },
       //   ],
       // },
-      // footer: {
-      //   style: 'dark',
-      //   links: [
-      //     {
-      //       title: 'Docs',
-      //       items: [
-      //         {
-      //           label: 'Docs',
-      //           to: '/docs/intro',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'Community',
-      //       items: [
-      //         {
-      //           label: 'Discord',
-      //           href: 'https://discordapp.com/invite/docusaurus',
-      //         },
-      //         {
-      //           label: 'Twitter',
-      //           href: 'https://twitter.com/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'More',
-      //       items: [
-      //         {
-      //           label: 'Blog',
-      //           to: '/blog',
-      //         },
-      //         {
-      //           label: 'GitHub',
-      //           href: 'https://github.com/jupyter-naas/naas',
-      //         },
-      //       ],
-      //     },
-      //   ],
-      //   copyright: `Copyright © ${new Date().getFullYear()} NaasAI, Inc.`,
-      // },
-      navbar: {
-      items: [
-        {
-          type: 'html',
-          position: 'right',
-          value: `<a href="https://workspace.naas.ai/login" target="_blank"><button>Sign in</button><a>`,
-        },
-      ],
-    },
       prism: {
         theme: darkCodeTheme,
       },
